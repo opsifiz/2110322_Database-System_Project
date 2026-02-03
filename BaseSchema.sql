c03705ed14e852335c169a68a7b26cafe8321960
Thyrei <pooky.thanisara@gmail.com>
2026-02-03 21:15:26 +0700

    timestamp in booking

--- BaseSchema.sql
+++ BaseSchema.sql
@@ -87,7 +87,7 @@ CREATE TABLE IF NOT EXISTS booking (
 
     start_date DATE NOT NULL,
     end_date DATE NOT NULL,
-
+	book_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT pk PRIMARY KEY (user_id, booking_id, campground_id, tent_id),
     CONSTRAINT check_dates CHECK (end_date > start_date),
     FOREIGN KEY (user_id) REFERENCES users(user_id),
