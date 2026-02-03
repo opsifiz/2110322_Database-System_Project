147376de3831170c3cc4ce87f0989d104fcde23b
MisterO <119954673+opsifiz@users.noreply.github.com>
2026-02-03 20:42:40 +0700

    Insert campground and tent data into Test.sql

--- FunctionalRequirements/3/Test.sql
+++ FunctionalRequirements/3/Test.sql
@@ -1,3 +1,58 @@
+-- ข้อ 3
+-- Campground Dataset
+INSERT INTO campgrounds (
+    house_no,
+    street,
+    subdistrict,
+    district,
+    province,
+    postal_code,
+    name,
+    map,
+    email
+) VALUES
+(
+    '123/4',
+    'ถนนภูเขา',
+    'ช้างเผือก',
+    'เมือง',
+    'เชียงใหม่',
+    '50300',
+    'Doi Mountain Camp',
+    'https://maps.google.com/?q=Doi+Mountain+Camp',
+    'contact@doimountaincamp.com'
+),
+(
+    '89',
+    'ถนนเลียบคลอง',
+    'คลองหนึ่ง',
+    'คลองหลวง',
+    'ปทุมธานี',
+    '12120',
+    'คลองหลวง Riverside Camp',
+    'https://maps.google.com/?q=Khlong+Luang+Riverside+Camp',
+    'info@riversidecamp.co.th'
+);
+
+-- Tent Dataset
+INSERT INTO tents (
+    campground_id,
+    tent_id,
+    size,
+    zone,
+    price
+) VALUES
+-- Campground 1
+(1, 1, 'S', 'Hill Zone', 500.00),
+(1, 2, 'M', 'Hill Zone', 800.00),
+(1, 3, 'L', 'Forest Zone', 1200.00),
+
+-- Campground 2
+(2, 1, 'S', 'River Zone', 600.00),
+(2, 2, 'M', 'River Zone', 900.00),
+(2, 3, 'L', 'Garden Zone', 1300.00);
+
+
 SELECT book_campground(
     1,          -- user_id
     2,          -- campground_id
